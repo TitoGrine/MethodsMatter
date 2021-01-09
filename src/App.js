bc0c08741c6abd935d61ed41f7161b1310708b68
TitoGrine <tito999@gmail.com>
2021-01-09 19:10:04 +0000

    Integrated app with google analytics.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import Map from "./components/Map";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import AboutButton from "./components/Button/AboutButton";
+import ReactGA from "react-ga";
 
 function App() {
+  ReactGA.initialize(process.env.REACT_APP_GOOGLE_TRACK_ID);
+  ReactGA.pageview("/");
+
   return (
     <div className="App">
       <Header />
